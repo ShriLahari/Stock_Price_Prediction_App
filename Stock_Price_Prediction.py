73250ebb2f4e8e5abfca6d855c4adb978fe54808
ShriLahari_T <146570092+ShriLahari@users.noreply.github.com>
2024-03-16 11:15:58 +0530

    Update Stock_Price_Prediction.py

--- Stock_Price_Prediction.py
+++ Stock_Price_Prediction.py
@@ -81,7 +81,7 @@ X_train, y_train = np.array(X_train), np.array(y_train)
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1)) 
 
 # Load the model
-my_model = load_model("Stock_Price_Prediction_App/blob/main/Stock_Price_Model_weights.h5")
+my_model = load_model("C:/Users/tshrilahari/Downloads/Stock_Price_Model_weights.h5")
 
 # Prepare test data
 X_test, y_test = [], []
